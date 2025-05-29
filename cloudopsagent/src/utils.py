87c57ec99ce07dc047b05ce374b447719fed469b
Tanmay <tannmay.chopade@lumiq.ai>
2025-05-29 15:38:20 +0530

    utils logs modified

--- cloudopsagent/src/utils.py
+++ cloudopsagent/src/utils.py
@@ -6,7 +6,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def get_secret(SecName):
-    logger.info("Retre")
+    logger.info(f"Retreiving secrets of: {SecName}")
     client = boto3.client("secretsmanager")
     try:
         response = client.get_secret_value(SecretId=SecName)

feb113e4206dca494a6c53c571c1ced4c414e3df
Tanmay <tannmay.chopade@lumiq.ai>
2025-05-29 18:16:36 +0530

    utils changed

--- cloudopsagent/src/utils.py
+++ cloudopsagent/src/utils.py
@@ -10,7 +10,7 @@ def get_secret(SecName):
     client = boto3.client("secretsmanager")
     try:
         response = client.get_secret_value(SecretId=SecName)
-        result = json.loads(response['SecretString'])
+        # result = json.loads(response['SecretString'])
         return str(response['SecretString'])
     except Exception as e:
         logger.info(f"Error fetching secret: {e}")
